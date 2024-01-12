d27485362cbf6b7a2897a4e4a4c46c45e3f82763
shon5544 <sbs8239@gmail.com>
2024-01-13 00:55:38 +0900

    fix: 접근 지정자 수정

--- src/main/java/com/gdsc/hack/dto/request/PostRequestDto.java
+++ src/main/java/com/gdsc/hack/dto/request/PostRequestDto.java
@@ -9,8 +9,8 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostRequestDto {
-    public String email;
-    public String title;
-    public String content;
-    public FoodMapRequestDto foodMap;
+    private String email;
+    private String title;
+    private String content;
+    private FoodMapRequestDto foodMap;
 }

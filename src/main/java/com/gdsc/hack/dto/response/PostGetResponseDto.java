76dc8a79276177d7f3aafd4a9312c19bae48d91f
shon5544 <sbs8239@gmail.com>
2024-01-13 04:38:46 +0900

    feat: Detail을 위한 response ˆdto 추가

---
+++ src/main/java/com/gdsc/hack/dto/response/PostGetResponseDto.java
@@ -0,0 +1,16 @@
+package com.gdsc.hack.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class PostGetResponseDto {
+	private Long post_id;
+	private String title;
+	private String content;
+	private String userName;
+	private FoodMapGetResponseDto foodMap;
+}

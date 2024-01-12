0bc6eb1cb67a89c52344f8a0bd66665c241fbad3
SangWoon123 <alrax3574@naver.com>
2024-01-12 18:05:36 +0900

    feat: user,favorite 도메인 추가

---
+++ src/main/java/com/gdsc/hack/domain/Favorite.java
@@ -0,0 +1,19 @@
+package com.gdsc.hack.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Favorite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
---
+++ src/main/java/com/gdsc/hack/domain/User.java
@@ -0,0 +1,18 @@
+package com.gdsc.hack.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nickname;
+    private String email;
+    private String password;
+}

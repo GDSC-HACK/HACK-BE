beb1eb10d5c90d5a8464eb2de989542ac3747cae
shon5544 <sbs8239@gmail.com>
2024-01-13 02:37:41 +0900

    feat: id, email 필드 추가 - email은 ArgumentResolver 쓸테니 없어질 필드임.

--- src/main/java/com/gdsc/hack/dto/request/MapNodeEditRequestDto.java
+++ src/main/java/com/gdsc/hack/dto/request/MapNodeEditRequestDto.java
@@ -1,6 +1,14 @@
 package com.gdsc.hack.dto.request;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MapNodeEditRequestDto {
+	private Long id;
+	private String email;
 	private String restaurantName;
 	private Double latitude;
 	private Double longitude;

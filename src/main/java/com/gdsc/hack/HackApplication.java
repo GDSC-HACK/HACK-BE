6e69a37941438f0a0e07048c7e3b8264891cfb19
shon5544 <sbs8239@gmail.com>
2024-01-12 16:35:00 +0900

    initial commit

---
+++ src/main/java/com/gdsc/hack/HackApplication.java
@@ -0,0 +1,13 @@
+package com.gdsc.hack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackApplication.class, args);
+	}
+
+}

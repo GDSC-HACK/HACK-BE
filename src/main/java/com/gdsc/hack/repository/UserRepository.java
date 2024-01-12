53ddfb884d206c7aca2c7ad2fd2bb11686ba42b7
shon5544 <sbs8239@gmail.com>
2024-01-12 21:18:10 +0900

    fix: findByNickname -> findByEmail

--- src/main/java/com/gdsc/hack/repository/UserRepository.java
+++ src/main/java/com/gdsc/hack/repository/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User,Long> {
 
     Boolean existsByNickname(String nickname);
-    User findByNickname(String nickname);
+    User findByEmail(String email);
 }

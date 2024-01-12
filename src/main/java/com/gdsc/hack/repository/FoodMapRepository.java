5222e00c6f3503c8ea9db4029b97000bcf4e639f
shon5544 <sbs8239@gmail.com>
2024-01-12 22:32:22 +0900

    feat: FoodMap, MapNode Repository 추가

---
+++ src/main/java/com/gdsc/hack/repository/FoodMapRepository.java
@@ -0,0 +1,7 @@
+package com.gdsc.hack.repository;
+
+import com.gdsc.hack.domain.FoodMap;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FoodMapRepository extends JpaRepository<FoodMap, Long> {
+}
---
+++ src/main/java/com/gdsc/hack/repository/MapNodeRepository.java
@@ -0,0 +1,7 @@
+package com.gdsc.hack.repository;
+
+import com.gdsc.hack.domain.MapNode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MapNodeRepository extends JpaRepository<MapNode, Long> {
+}
